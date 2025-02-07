d48e9def7b0fbbdf8ad08c9481e7c29864b028f9
Ushan Jayakody <ushanridmaransara@gmail.com>
2025-02-07 15:29:57 +0530

    Home page design

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,28 @@
+<div class="w-full bg-gradient-to-r from-blue-200 to-cyan-200 py-6 px-4 sm:px-6 lg:px-8 mx-auto h-auto md:h-[70vh] lg:h-[60vh] flex items-center">
+  <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+    <!-- Grid -->
+    <div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
+      <!-- Left Column -->
+      <div>
+        <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">
+          Smart Way to Order Gas! <span class="text-blue-600">GasByGas</span>
+        </h1>
+        <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">
+          Order Gas Online - Fast, Safe, Reliable.
+        </p>
+      </div>
+
+      <!-- Right Column (Image) -->
+      <div class="flex justify-end">
+        <div class="relative">
+          <img class="w-full max-w-md rounded-md" 
+               src="https://png.pngtree.com/png-vector/20220630/ourmid/pngtree-cylinders-gas-cylinder-lpg-propane-png-image_5652012.png" 
+               alt="Image Description">
+          <div class="absolute inset-0 -z-[1] bg-gradient-to-tr from-gray-200 via-white/0 to-white/0 w-full h-full rounded-md mt-4 -mb-4 me-4 -ms-4 lg:mt-6 lg:-mb-6 lg:me-6 lg:-ms-6 dark:from-slate-800 dark:via-slate-900/0 dark:to-slate-900/0"></div>
+        </div>
+      </div>
+      <!-- End Right Column -->
+    </div>
+    <!-- End Grid -->
+  </div>
+</div>
